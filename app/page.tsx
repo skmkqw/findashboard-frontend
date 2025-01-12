eddaafdc7e2f968b928599eb03702ca178f9ed97
skmkqw <tkorsakov77@gmail.com>
2025-01-12 20:33:55 +0100

    using mode toggle on initial page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <h1>Welcome to ZBank!</h1>
+      <ModeToggle />
+    </div>
   );
 }
