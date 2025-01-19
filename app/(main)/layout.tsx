5ebf9f075a7d0e9390a98d001ffa7f5472ba40d6
skmkqw <tkorsakov77@gmail.com>
2025-01-19 21:15:54 +0100

    adjusted main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import { Kode_Mono } from "next/font/google";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { ModeToggle } from "@/components/common/mode-toggle";
+import NavUser from "@/components/nav-user";
 
 export const metadata: Metadata = {
     title: "ZBank",
@@ -37,13 +38,15 @@ export default function RootLayout({
                     >
                         <AppSidebar />
                         <SidebarInset>
-                            <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
+                            <header className="sticky top-0 flex shrink-0 items-center justify-between gap-2 border-b bg-background p-4">
                             <SidebarTrigger className="-ml-1" />
-                            <ModeToggle />
+                            <div className="flex items-center gap-4">
+                                <ModeToggle />
+                                <NavUser />
+                            </div> 
                             </header>
                             <div className="flex flex-1 flex-col gap-4 p-4">
                                 <main>
-                                <SidebarTrigger />
                                     {children}
                                 </main>
                             {Array.from({ length: 24 }).map((_, index) => (
