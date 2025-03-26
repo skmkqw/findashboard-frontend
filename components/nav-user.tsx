d84fa185d8d6e4a242900ada01d2f4ea47605e87
skmkqw <tkorsakov77@gmail.com>
2025-03-26 22:32:56 +0100

    use real user data in nav-user component

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useAuthStore } from "@/stores/auth-store";
 import { User } from "lucide-react"
 
 export default function NavUser() {
+  const { user } = useAuthStore();
+
   return (
     <div className="flex items-center gap-3">
-      <span className="font-medium">John Doe</span>
+      <span className="font-medium">{`${user?.firstName} ${user?.lastName}`}</span>
       <User className="h-[1.4rem] w-[1.4rem]" />
     </div>
   )
