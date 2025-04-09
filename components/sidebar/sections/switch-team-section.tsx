92350887e349804a3cc4d246e2e9963426efbb84
skmkqw <tkorsakov77@gmail.com>
2025-04-09 20:12:52 +0200

    rebuild switch-team section

--- components/sidebar/sections/switch-team-section.tsx
+++ components/sidebar/sections/switch-team-section.tsx
@@ -1,27 +1,60 @@
-import { useTeamStore } from '@/stores/team-store';
+import { Button } from '@/components/ui/button';
+import { Team, useTeamStore } from '@/stores/team-store';
 import React from 'react';
 
+interface TeamCardProps {
+  team: Team;
+  isActive: boolean;
+  handleClick: (team: Team) => void;
+}
+
+const TeamCard: React.FC<TeamCardProps> = ({ team, isActive, handleClick }) => {
+  return (
+    <a
+      onClick={() => handleClick(team)}
+      className={`flex flex-col items-start gap-2 border-b p-4 text-sm leading-tight hover:bg-sidebar-accent hover:text-sidebar-accent-foreground cursor-pointer
+      ${isActive ? "bg-sidebar-accent text-sidebar-accent-foreground" : ""}
+    `}
+    >
+      <div className="font-medium">{team.name}</div>
+      <div className="mt-1 text-xs text-muted-foreground">{team.description}</div>
+    </a>
+  );
+}
+
 export const SwitchTeamSection: React.FC = () => {
-  const { teams, activeTeam, switchTeam } = useTeamStore();
+  const { personalSpace, teams, activeTeam, switchTeam } = useTeamStore();
 
   return (
     <>
-      {teams.map((team) => {
-        const isActive = activeTeam?.id === team.id;
-
-        return (
-          <a
-            key={team.id}
-            onClick={() => switchTeam(team)}
-            className={`flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground cursor-pointer
-              ${isActive ? "bg-sidebar-accent text-sidebar-accent-foreground" : ""}
-            `}
-          >
-            <div className="font-medium">{team.name}</div>
-            <div className="mt-1 text-xs text-muted-foreground">{team.description}</div>
-          </a>
-        );
-      })}
+      {personalSpace !== null ?
+        <TeamCard
+          team={personalSpace}
+          isActive={activeTeam?.id === personalSpace.id}
+          handleClick={switchTeam}
+        />
+        :
+        <div className="p-4 flex flex-col items-center gap-3 text-center text-sm border-b">
+          <p>You don't have a personal space!</p>
+          <Button className="w-full text-xs">Create</Button>
+        </div>
+      }
+      {teams.length === 0 ?
+        <div className="p-4 flex flex-col items-center gap-3 text-center text-sm border-b">
+          <p>You don't have any teams!</p>
+          <Button className="w-full text-xs">Create</Button>
+        </div>
+        :
+        teams.map((team) => {
+          return (
+            <TeamCard
+              key={team.id}
+              team={team}
+              isActive={activeTeam?.id === team.id}
+              handleClick={switchTeam}
+            />
+          );
+        })}
     </>
   );
 };
